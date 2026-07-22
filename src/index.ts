87e51f65b22de9028975fedc9a9b6ff3b95fe80b
Marcello Venturini <marcellofventurini@gmail.com>
2026-07-22 15:29:20 -0300

    feat: create a mock db and a method to get all products

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,23 @@
-import { serve } from '@hono/node-server'
-import { Hono } from 'hono'
+import { serve } from '@hono/node-server';
+import { Hono } from 'hono';
 
-const app = new Hono()
+const app = new Hono();
 
-app.get('/', (c) => {
-  return c.text('Hello Hono!')
-})
+const produtcs = [
+  { id: '1', name: 'Teclado', price: 350 },
+  { id: '2', name: 'Mouse', price: 200 },
+];
 
-serve({
-  fetch: app.fetch,
-  port: 3000
-}, (info) => {
-  console.log(`Server is running on http://localhost:${info.port}`)
-})
+app.get('/products', (c) => {
+  return c.json(produtcs);
+});
+
+serve(
+  {
+    fetch: app.fetch,
+    port: 3000,
+  },
+  (info) => {
+    console.log(`Server is running on http://localhost:${info.port}`);
+  },
+);
