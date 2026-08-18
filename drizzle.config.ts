8583294eb7ae856b1f78cc24abdf86d06c70f47d
Marcello Venturini <marcellofventurini@gmail.com>
2026-08-18 16:44:01 -0300

    fix: change the entry point for schemas and update syntax and import for db config file

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -2,7 +2,7 @@ import 'dotenv/config';
 import { defineConfig } from 'drizzle-kit';
 
 export default defineConfig({
-  schema: './src/db/schemas/*',
+  schema: './src/db/schemas/index.schemas.ts',
   out: './drizzle',
   dialect: 'sqlite',
   dbCredentials: {
--- src/db/index.db.ts
+++ src/db/index.db.ts
@@ -1,9 +1,9 @@
 import Database from 'better-sqlite3';
 import 'dotenv/config';
-import { drizzle } from 'drizzle-orm/singlestore';
+import { drizzle } from 'drizzle-orm/better-sqlite3';
 
 // abre a conexão com o sqlite
 const sqlite = new Database(process.env.DB_URL!);
 
 // instancia o cliente do Drizzle passando a conexão
-export const db = drizzle(sqlite)
+export const db = drizzle({ client: sqlite });
